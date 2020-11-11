d8d758175ee1fe426f6a8f0d7954f517cdafc31b
lord sayur <omar.ibrahim@dynamiktechnologies.com.bn>
2020-11-11 01:11:10 +0000

    add test case step execution notes component

---
+++ src/components/TCStepExecutionNotes.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-row>
+    <v-col class="mb-4">
+      <h2>Test Case Steps Execution Notes</h2>
+      <v-data-table
+        dense
+        :headers="headers"
+        :items="executions"
+        item-key="name"
+        class="elevation-1"
+      ></v-data-table>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "TCStepExecutionNotes",
+
+  data: () => ({
+    executions: [],
+    headers: [
+      {
+        text: "DateTime",
+        align: "start",
+        sortable: false,
+        value: "DateTime",
+      },
+      { text: "Test Case", value: "Test Case" },
+      { text: "Test Plan", value: "Test Plan" },
+      { text: "Test Suite", value: "Test Suite" },
+      { text: "action", value: "action" },
+      { text: "execution note", value: "execution note" },
+      { text: "expected result", value: "expected result" },
+      { text: "step no", value: "step no" },
+      { text: "TC id", value: "test case eid" },
+      { text: "tester", value: "tester" },
+    ],
+  }),
+
+  async created() {
+    let response = await fetch("/api/stepExecutionNotes");
+    let data = await response.json();
+    this.executions = data;
+    console.log("stepExecutionNotes: ", this.executions);
+  },
+};
+</script>
