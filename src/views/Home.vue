55416a887c0e046880256a27ea23f8a8aabfe5b9
lord sayur <omar.ibrahim@dynamiktechnologies.com.bn>
2020-11-12 02:16:31 +0000

    Add tab layout on home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,11 @@
   <v-container>
     <h1 class="display-2 font-weight-bold mb-3">TestLink Reports</h1>
     <hr />
-    <TCExecutionNotes />
-    <TCStepExecutionNotes />
+    <v-btn class="mt-2" @click="updateButton" color="primary"
+      >View {{ tableName }}</v-btn
+    >
+    <TCExecutionNotes v-show="tableState" />
+    <TCStepExecutionNotes v-show="!tableState" />
   </v-container>
 </template>
 
@@ -17,6 +20,20 @@ export default {
   components: {
     TCExecutionNotes,
     TCStepExecutionNotes,
+  },
+  data() {
+    return {
+      tableName: "TC Step Execution Notes",
+      tableState: true,
+    };
+  },
+  methods: {
+    updateButton() {
+      this.tableName = this.tableState
+        ? "TC Execution Notes"
+        : "TC Step Execution Notes";
+      this.tableState = !this.tableState;
+    },
   },
 };
 </script>
