ab99c2bca74a03285781207ed9cce1601208f453
lord sayur <omar.ibrahim@dynamiktechnologies.com.bn>
2021-01-06 16:02:59 +0800

    add filter by test plan for TC execution note

--- src/components/TCExecutionNotes.vue
+++ src/components/TCExecutionNotes.vue
@@ -1,23 +1,42 @@
 <template>
-  <v-row>
-    <v-col class="mb-4">
-      <h2>Test Case Execution Notes</h2>
-      <v-btn
-        class="mt-2"
-        :disabled="canDownload"
-        @click="downloadCSV"
-        color="primary"
-        >Export</v-btn
-      >
-      <v-data-table
-        dense
-        :headers="headers"
-        :items="tableData"
-        item-key="name"
-        class="elevation-1"
-      ></v-data-table>
-    </v-col>
-  </v-row>
+  <div>
+    <v-row>
+      <v-col>
+        <h2>Test Case Execution Notes</h2>
+        <v-select
+          v-model="value"
+          :items="items"
+          item-text="test_plan_name"
+          item-value="test_plan_id"
+          attach
+          chips
+          label="Chips"
+          multiple
+        ></v-select>
+        <v-btn class="mt-2" @click="fetchExecutionNotes" color="primary"
+          >Search</v-btn
+        >
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn
+          class="mt-2"
+          :disabled="canDownload"
+          @click="downloadCSV"
+          color="primary"
+          >Export</v-btn
+        >
+        <v-data-table
+          dense
+          :headers="headers"
+          :items="tableData"
+          item-key="name"
+          class="elevation-1"
+        ></v-data-table>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -30,6 +49,8 @@ export default {
 
   data: () => ({
     executions: [],
+    items: [],
+    value: [],
     tableData: [],
     headers: [
       {
@@ -50,14 +71,34 @@ export default {
   }),
 
   async created() {
-    let response = await fetch("/api/executionNotes");
-    let data = await response.json();
-    this.executions = data;
-
-    this.tableData = this.cleanData(this.executions);
+    await this.fetchTestPlan();
   },
 
   methods: {
+    async fetchTestPlan() {
+      try {
+        let response = await fetch("/api/getTestPlans");
+        let data = await response.json();
+        this.value = [];
+        this.items = data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async fetchExecutionNotes() {
+      try {
+        this.tableData = [];
+        let response = await fetch(
+          "/api/executionNotes?test_plan_id=" + this.value.toString()
+        );
+        let data = await response.json();
+        console.log(data);
+        this.executions = data;
+        this.tableData = this.cleanData(this.executions);
+      } catch (error) {
+        console.error(error);
+      }
+    },
     cleanData(originalData) {
       let cleanedData = originalData;
       cleanedData.forEach((data, index) => {
